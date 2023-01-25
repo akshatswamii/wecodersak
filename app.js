d55d32bd582d1a1906cc6326d4eab5ba61c326a4
Akshat <akshat.22503@sscbs.du.ac.in>
2023-01-25 16:30:31 +0530

    start

---
+++ app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwq-hJZN4CHx0BocOaCZMS7LESIP0Sb7Y",
+    authDomain: "we-coders.firebaseapp.com",
+    projectId: "we-coders",
+    storageBucket: "we-coders.appspot.com",
+    messagingSenderId: "302077807764",
+    appId: "1:302077807764:web:c45f9a32085cf68505b881",
+    measurementId: "G-Q1JDPGV1WW"
+  };
